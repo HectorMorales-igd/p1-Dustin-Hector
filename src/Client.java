7b1f588fdbb17e251f76e9ce120ede2348df5159
HectorMorales-igd <hmv3alumnes@gmail.com>
2026-05-01 19:38:25 +0200

    Client4.0

--- src/Client.java
+++ src/Client.java
@@ -6,9 +6,24 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Client {
+        private static final String DEFAULT_HOST = "localhost";
+        private static final int DEFAULT_PORT = 12345;
         public static void main(String[] args) {
-                String host = "localhost";
-                int port = 12345;
+                String host = DEFAULT_HOST;
+                int port = DEFAULT_PORT;
+
+                if (args.length >= 1) {
+                        host = args[0];
+                }
+
+                if (args.length >= 2) {
+                        try {
+                                port = Integer.parseInt(args[1]);
+                        } catch (NumberFormatException nfe) {
+                                System.err.println("Port no vàlid, s'utilitza " + DEFAULT_PORT);
+                                port = DEFAULT_PORT;
+                        }
+                }
 
                 Client client = new Client();
                 client.run(host, port);
