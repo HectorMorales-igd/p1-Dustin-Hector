ca4f0c26f67d82e3eca2d0d76e7531eaec59543a
dustinsr <dustindusaro5@gmail.com>
2026-05-01 16:04:10 +0200

    Afegir gestio concurrent de clients amb un fi per connexio

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,12 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class Server {
 
@@ -21,7 +27,8 @@ public class Server {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Client connectat: " + clientSocket.getRemoteSocketAddress());
 
-                clientSocket.close();
+                Thread t = new Thread(new ClientHandler(clientSocket, videoGamesDB));
+                t.start();
             }
         }
     }
@@ -32,6 +39,69 @@ public class Server {
             server.start();
         } catch (IOException ioe) {
             System.err.println("Error iniciant servidor / obrint base de dades!");
+        }
+    }
+
+    private static class ClientHandler implements Runnable {
+
+        private final Socket socket;
+        private final VideoGamesDB videoGamesDB;
+
+        ClientHandler(Socket socket, VideoGamesDB videoGamesDB) {
+            this.socket = socket;
+            this.videoGamesDB = videoGamesDB;
+        }
+
+        @Override
+        public void run() {
+            DataInputStream in = null;
+            DataOutputStream out = null;
+
+            try {
+                in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+                out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+
+                boolean fi = false;
+
+                while (!fi) {
+                    int option;
+                    try {
+                        option = in.readInt();
+                    } catch (EOFException e) {
+                        break;
+                    }
+
+                    System.out.println("Client " + socket.getRemoteSocketAddress() + " opció " + option);
+
+                    switch (option) {
+                        case 5:
+                            fi = true;
+                            break;
+                        default:
+                            fi = true;
+                            break;
+                    }
+                }
+
+            } catch (SocketException | EOFException e) {
+                // client desconnectat
+            } catch (IOException e) {
+                System.err.println("Error de comunicació amb el client " + socket.getRemoteSocketAddress());
+            } finally {
+                closeQuietly(in);
+                closeQuietly(out);
+                closeQuietly(socket);
+                System.out.println("Client desconnectat: " + socket.getRemoteSocketAddress());
+            }
+        }
+
+        private void closeQuietly(AutoCloseable c) {
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (Exception ignored) {
+                }
+            }
         }
     }
 }
